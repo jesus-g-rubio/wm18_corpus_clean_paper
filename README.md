e4ab61df267243eeb54555eaf3395f6025d65898
Jesús González-Rubio <jesus.g@webinterpret.com>
2018-06-25 15:40:28 +0200

    skeleton of the paper

--- README.md
+++
@@ -1,2 +0,0 @@
-# wm18_corpus_clean_paper
-Paper for the WMT18 corpus cleaning task
---
+++ compile.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+fn='main'
+
+rm ${fn}.aux ${fn}.bbl ${fn}.blg ${fn}.log ${fn}.out ${fn}.pdf
+
+pdflatex ${fn}.tex
+bibtex ${fn}
+pdflatex ${fn}.tex
+pdflatex ${fn}.tex
