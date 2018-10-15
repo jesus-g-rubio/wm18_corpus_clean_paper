18fe03e7eaa1702cf3ad8aaa4f2f9cdb2d989fc4
Jesús González-Rubio <jesus.g@webinterpret.com>
2018-10-15 10:48:07 +0200

    addedd bibliography

--- poster/compile.sh
+++ poster/compile.sh
@@ -4,7 +4,7 @@ fn='poster'
 
 rm ${fn}.aux ${fn}.bbl ${fn}.blg ${fn}.log ${fn}.out ${fn}.pdf
 
-#pdflatex ${fn}.tex
-#bibtex ${fn}
+pdflatex ${fn}.tex
+bibtex ${fn}
 pdflatex ${fn}.tex
 pdflatex ${fn}.tex
